a77f57645ef5bd3a8005523a43b1059ea485651d
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-02 02:40:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,3 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN").text
-
-coursese = doc.css("inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
